e0c90955a53c3d16ba42599aef4a4b3d0f8138f9
ariqyandri <ariq140199@gmail.com>
2021-01-21 02:47:29 +0100

    Set up relations in roomFacility

--- migrations/20210121005217-create-room-facility.js
+++ migrations/20210121005217-create-room-facility.js
@@ -1,30 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('roomFacilities', {
+    await queryInterface.createTable("roomFacilities", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       roomId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "rooms",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
       },
       facilityId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "facilities",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('roomFacilities');
-  }
+    await queryInterface.dropTable("roomFacilities");
+  },
 };
