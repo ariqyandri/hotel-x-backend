ad9d105564765956c10d8a4a4ba970ac4162b815
ariqyandri <ariq140199@gmail.com>
2021-01-21 01:47:00 +0100

    Generate Picture model

---
+++ migrations/20210121004346-create-picture.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Pictures', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      caption: {
+        type: Sequelize.TEXT
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      iconUrl: {
+        type: Sequelize.STRING
+      },
+      thumbnailUrl: {
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Pictures');
+  }
+};
---
+++ models/picture.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Picture extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Picture.init({
+    title: DataTypes.STRING,
+    caption: DataTypes.TEXT,
+    url: DataTypes.STRING,
+    iconUrl: DataTypes.STRING,
+    thumbnailUrl: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Picture',
+  });
+  return Picture;
+};
