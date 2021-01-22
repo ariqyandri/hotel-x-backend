16a055a24cd917748aae6920110e69a3f7e9b2e8
ariqyandri <ariq140199@gmail.com>
2021-01-22 04:00:44 +0100

    Update

--- routers/categories.js
+++ routers/categories.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const Categories = require("../models").category;
+const About = require("../models").about;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const categories = await Categories.findAll();
-    res.status(200).send(categories);
+    const about = await About.findAll();
+    res.status(200).send({ categories, about });
   } catch (error) {
     next(error);
   }
